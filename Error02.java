376da6730aa221589644e43d8ade0db2a58d15a2
Dawson Deere <ddeere@csisd.org>
2022-09-16 08:33:01 -0500

    Update Error02.java

--- Error02.java
+++ Error02.java
@@ -3,14 +3,17 @@ import java.util.Scanner;
 public class Error02 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter your first name: ");
-    String name = in.nextLine();
-    int pos = name.indexOf('e');
+    System.out.print("Enter your full name: ");
+    String name = in.next();
+    int pos = name.indexOf(' ');
     String sub = name.substring(0, pos);
     System.out.println(sub);
   }
 }
 /*
 Use input
+Will Smith
+
+Expected output:
 Will
 */
