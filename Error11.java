e0d3444a225a2a97bfc0fc1bee8c3ac71e2a9269
Dawson Deere <ddeere@csisd.org>
2022-09-16 08:41:52 -0500

    Update Error11.java

--- Error11.java
+++ Error11.java
@@ -2,4 +2,6 @@ public class Error11.java {
   public static void main(String[] args) {
     String name = "Will;
     double costApples = 2.50;
+    System.out.println(name);
+    System.out.println(costApples);
   }
