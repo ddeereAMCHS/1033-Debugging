0a995ec0dc09cec209e46c5fdd52dda6a196b487
Dawson Deere <ddeere@csisd.org>
2022-09-16 08:21:08 -0500

    Update Error04.java

--- Error04.java
+++ Error04.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Error4 {
+public class Error04 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int age = in.nextLine();
