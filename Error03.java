816bf9d757471d73622bb7ca97bf7dee3045be1b
Dawson Deere <ddeere@csisd.org>
2023-08-31 08:59:15 -0500

    added input prompt

--- Error03.java
+++ Error03.java
@@ -1,6 +1,7 @@
 public class Error03 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter your age: ");
     int age = Integer.parseInt(in.nextLine());
     System.out.println(Age);
   }
