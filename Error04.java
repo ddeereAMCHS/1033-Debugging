4f189355dcd2446bb96fbaeedfe523e4e84f8e1a
Dawson Deere <ddeere@csisd.org>
2023-08-31 08:59:46 -0500

    added input prompt

--- Error04.java
+++ Error04.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Error04 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter your age: ");
     int age = in.nextLine();
     System.out.println("Your age is " age);
   }
