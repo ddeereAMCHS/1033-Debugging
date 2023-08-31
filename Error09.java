172e17cf01a1f19617ed48f1493020028b706d0a
Dawson Deere <ddeere@csisd.org>
2023-08-31 09:00:22 -0500

    added input prompt

--- Error09.java
+++ Error09.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Error09 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter your full name: ");
     String fullName = in.nextLine();
     String firstName = fullname.substring(0, " ");
     System.out.println(firstName)
